be3725015597b28a6c9d705c134448825ccc38f3
Sepehr Frd <sepehr.fr1@gmail.com>
2022-06-03 08:52:55 +0430

    Setup Entity Framework

---
+++ Program.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Diagnostics;
+using NLog;
+using NLog.Web;
+using Microsoft.EntityFrameworkCore;
+using UserAssignmentRedone.Models;
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Info("Application started running.");
+
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+    
+    // Entity Framework: Setup Entity Framework for Dependency injection
+    
+    builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+
+    app.UseExceptionHandler(appBuilder =>
+    {
+        appBuilder.Run(async context =>
+        {
+            var feature = context.Features.Get<IExceptionHandlerFeature>();
+            
+            if (feature.Error is not null)
+            {
+                logger.Error(feature.Error, "Exception thrown here!");
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(feature.Error.Message);
+            }
+        });
+    });
+    
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
+
+
+
+
+
+
+
+
